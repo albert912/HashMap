ba7dd6e117acaf87c83584b666ca9d29d8e13025
albert912 <aatunckol@gmail.com>
2025-02-09 12:42:08 +0300

    Declare hash function using modulo

--- hashmap.js
+++ hashmap.js
@@ -5,7 +5,17 @@ constructor() {
         }
 
 
-
+        function hash(key) {
+            let hashCode = 0;
+               
+            const primeNumber = 31;
+            for (let i = 0; i < key.length; i++) {
+              hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            }
+         
+            return hashCode % this.buckets.length;
+          } 
+         
 
 
 
