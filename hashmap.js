94e4b340b49bfd64bfb8c3902f59287e35dab651
albert912 <aatunckol@gmail.com>
2025-02-09 12:19:53 +0300

    Declare capacity and load factor

---
+++ hashmap.js
@@ -0,0 +1,47 @@
+class Hashmap() {
+
+constructor() {
+ this.buckets = new Array(16);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    const hashMap = new HashMap({ loadFactor: 0.5 });
+
+
+
+
+
+
+
+
+
+
+
+
+
